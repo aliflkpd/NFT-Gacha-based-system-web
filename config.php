c200d95390f390a1c4139e148e3c6919941f0a8b
Aliflkpd <andialifnurman@gmail.com>
2022-06-22 00:19:12 +0800

    another one

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
-<?php 
-mysql_connect('localhost','root','');
-mysql_select_db('tb_user');
+<?php
+$servername = "localhost";
+$username = "root"; // sesuaikan dengan username db masing-masing
+$password = ""; // sesuaikan dengan password db masing-masing
+$db = "tb_user"; // sesuaikan dengan password db masing-masing
+ 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+ 
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
 ?>
