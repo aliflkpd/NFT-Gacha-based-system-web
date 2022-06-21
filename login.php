374946d1eb84e869f09f89aabff3b2062805c17a
Aliflkpd <andialifnurman@gmail.com>
2022-06-22 00:33:52 +0800

    lanjut besok

--- login.php
+++ login.php
@@ -1,19 +1,30 @@
-<?php 
-include 'config.php';
- 
-$wallet = $_POST['wallet'];
-$password = md5($_POST['password']);
- 
-$login = mysql_query("select * from user where wallet='$wallet' and password='$password'");
-$cek = mysql_num_rows($login);
- 
-if($cek > 0){
-	session_start();
-	$_SESSION['wallet'] = $wallet;
-	$_SESSION['status'] = "login";
-	header("location:admin/index.php");
-}else{
-	header("location:index.php");	
+<?php
+session_start();
+require "config.php";
+if(isset($_POST['Wallet'])) {
+    $login = false;
+    $userLogin;
+    $query = $conn->query("
+        select*from user where 
+            Wallet='".$_POST['Wallet']);
+    while($user = $query->fetch_array()) {
+        if(password_verify($_POST['password'], $user['password'])) {
+            $login = true;
+            $userLogin = $user;
+        }
+    }
+    print_r($user);
+    if($login) {
+        $_SESSION['Wallet'] = $wallet;
+        $_SESSION['status'] = 'login';
+        header("Location: http://localhost/kuliah");
+    } else {
+        echo "Wallet or password is invalid!";
+    }
 }
- 
+
+if(isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: http://localhost/kuliah");
+}
 ?>
